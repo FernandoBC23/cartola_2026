0e8395b17228495432a82de94c385317a610dbd9
Fernando Chagas <fernandobchagas@gmail.com>
2026-03-11 19:19:16 -0300

    Atualiza datasets e rankings de scouts

--- scouts/datasets_scouts/media_pontos_clubes.js
+++ scouts/datasets_scouts/media_pontos_clubes.js
@@ -1,162 +1,162 @@
 const mediaPontosTimes = [
   {
     "Clube ID": "PAL",
-    "Media Pontos por Jogo": 2.1785714285714284,
-    "Media Pontos por Jogo Casa": 2.3333333333333335,
+    "Media Pontos por Jogo": 2.5,
+    "Media Pontos por Jogo Casa": 3.0,
     "Media Pontos por Jogo Fora": 2.0,
-    "Total Pontos Casa": 35,
-    "Total Pontos Fora": 26
-  },
-  {
-    "Clube ID": "FLA",
-    "Media Pontos por Jogo": 2.1785714285714284,
-    "Media Pontos por Jogo Casa": 2.3333333333333335,
-    "Media Pontos por Jogo Fora": 2.0,
-    "Total Pontos Casa": 35,
-    "Total Pontos Fora": 26
-  },
-  {
-    "Clube ID": "CRU",
-    "Media Pontos por Jogo": 1.9310344827586208,
-    "Media Pontos por Jogo Casa": 2.3333333333333335,
-    "Media Pontos por Jogo Fora": 1.5,
-    "Total Pontos Casa": 35,
-    "Total Pontos Fora": 21
-  },
-  {
-    "Clube ID": "MIR",
-    "Media Pontos por Jogo": 1.793103448275862,
-    "Media Pontos por Jogo Casa": 2.3333333333333335,
-    "Media Pontos por Jogo Fora": 1.2142857142857142,
-    "Total Pontos Casa": 35,
-    "Total Pontos Fora": 17
-  },
-  {
-    "Clube ID": "BOT",
-    "Media Pontos por Jogo": 1.5862068965517242,
-    "Media Pontos por Jogo Casa": 1.7857142857142858,
-    "Media Pontos por Jogo Fora": 1.4,
-    "Total Pontos Casa": 25,
-    "Total Pontos Fora": 21
-  },
-  {
-    "Clube ID": "BAH",
-    "Media Pontos por Jogo": 1.6428571428571428,
-    "Media Pontos por Jogo Casa": 2.357142857142857,
-    "Media Pontos por Jogo Fora": 0.9285714285714286,
-    "Total Pontos Casa": 33,
-    "Total Pontos Fora": 13
-  },
-  {
-    "Clube ID": "FLU",
-    "Media Pontos por Jogo": 1.4642857142857142,
-    "Media Pontos por Jogo Casa": 2.1538461538461537,
-    "Media Pontos por Jogo Fora": 0.8666666666666667,
-    "Total Pontos Casa": 28,
-    "Total Pontos Fora": 13
-  },
-  {
-    "Clube ID": "VAS",
-    "Media Pontos por Jogo": 1.3448275862068966,
-    "Media Pontos por Jogo Casa": 1.6666666666666667,
-    "Media Pontos por Jogo Fora": 1.0,
-    "Total Pontos Casa": 25,
-    "Total Pontos Fora": 14
+    "Total Pontos Casa": 6,
+    "Total Pontos Fora": 4
   },
   {
     "Clube ID": "SAO",
-    "Media Pontos por Jogo": 1.3103448275862069,
-    "Media Pontos por Jogo Casa": 1.7142857142857142,
-    "Media Pontos por Jogo Fora": 0.9333333333333333,
-    "Total Pontos Casa": 24,
-    "Total Pontos Fora": 14
-  },
-  {
-    "Clube ID": "RBB",
-    "Media Pontos por Jogo": 1.2413793103448276,
-    "Media Pontos por Jogo Casa": 1.5714285714285714,
-    "Media Pontos por Jogo Fora": 0.9333333333333333,
-    "Total Pontos Casa": 22,
-    "Total Pontos Fora": 14
+    "Media Pontos por Jogo": 2.5,
+    "Media Pontos por Jogo Casa": 3.0,
+    "Media Pontos por Jogo Fora": 2.0,
+    "Total Pontos Casa": 6,
+    "Total Pontos Fora": 4
   },
   {
     "Clube ID": "COR",
-    "Media Pontos por Jogo": 1.2413793103448276,
-    "Media Pontos por Jogo Casa": 1.5714285714285714,
-    "Media Pontos por Jogo Fora": 0.9333333333333333,
-    "Total Pontos Casa": 22,
-    "Total Pontos Fora": 14
+    "Media Pontos por Jogo": 1.75,
+    "Media Pontos por Jogo Casa": 1.5,
+    "Media Pontos por Jogo Fora": 2.0,
+    "Total Pontos Casa": 3,
+    "Total Pontos Fora": 4
+  },
+  {
+    "Clube ID": "BAH",
+    "Media Pontos por Jogo": 2.3333333333333335,
+    "Media Pontos por Jogo Casa": 1.0,
+    "Media Pontos por Jogo Fora": 3.0,
+    "Total Pontos Casa": 1,
+    "Total Pontos Fora": 6
+  },
+  {
+    "Clube ID": "FLU",
+    "Media Pontos por Jogo": 1.75,
+    "Media Pontos por Jogo Casa": 3.0,
+    "Media Pontos por Jogo Fora": 0.5,
+    "Total Pontos Casa": 6,
+    "Total Pontos Fora": 1
+  },
+  {
+    "Clube ID": "CAP",
+    "Media Pontos por Jogo": 1.75,
+    "Media Pontos por Jogo Casa": 1.5,
+    "Media Pontos por Jogo Fora": 2.0,
+    "Total Pontos Casa": 3,
+    "Total Pontos Fora": 4
+  },
+  {
+    "Clube ID": "RBB",
+    "Media Pontos por Jogo": 1.75,
+    "Media Pontos por Jogo Casa": 2.0,
+    "Media Pontos por Jogo Fora": 1.5,
+    "Total Pontos Casa": 4,
+    "Total Pontos Fora": 3
   },
   {
     "Clube ID": "GRE",
-    "Media Pontos por Jogo": 1.2413793103448276,
-    "Media Pontos por Jogo Casa": 1.6428571428571428,
-    "Media Pontos por Jogo Fora": 0.8666666666666667,
-    "Total Pontos Casa": 23,
-    "Total Pontos Fora": 13
+    "Media Pontos por Jogo": 1.5,
+    "Media Pontos por Jogo Casa": 3.0,
+    "Media Pontos por Jogo Fora": 0.0,
+    "Total Pontos Casa": 6,
+    "Total Pontos Fora": 0
   },
   {
-    "Clube ID": "CEA",
-    "Media Pontos por Jogo": 1.25,
-    "Media Pontos por Jogo Casa": 1.5,
+    "Clube ID": "MIR",
+    "Media Pontos por Jogo": 1.5,
+    "Media Pontos por Jogo Casa": 1.6666666666666667,
     "Media Pontos por Jogo Fora": 1.0,
-    "Total Pontos Casa": 21,
-    "Total Pontos Fora": 14
+    "Total Pontos Casa": 5,
+    "Total Pontos Fora": 1
   },
   {
-    "Clube ID": "INT",
-    "Media Pontos por Jogo": 1.25,
-    "Media Pontos por Jogo Casa": 1.6,
-    "Media Pontos por Jogo Fora": 0.8461538461538461,
-    "Total Pontos Casa": 24,
-    "Total Pontos Fora": 11
-  },
-  {
-    "Clube ID": "CAM",
-    "Media Pontos por Jogo": 1.1785714285714286,
-    "Media Pontos por Jogo Casa": 1.8461538461538463,
-    "Media Pontos por Jogo Fora": 0.6,
-    "Total Pontos Casa": 24,
-    "Total Pontos Fora": 9
+    "Clube ID": "CHA",
+    "Media Pontos por Jogo": 1.6666666666666667,
+    "Media Pontos por Jogo Casa": 2.0,
+    "Media Pontos por Jogo Fora": 1.0,
+    "Total Pontos Casa": 4,
+    "Total Pontos Fora": 1
   },
   {
     "Clube ID": "SAN",
-    "Media Pontos por Jogo": 1.1071428571428572,
-    "Media Pontos por Jogo Casa": 1.3571428571428572,
-    "Media Pontos por Jogo Fora": 0.8571428571428571,
-    "Total Pontos Casa": 19,
-    "Total Pontos Fora": 12
+    "Media Pontos por Jogo": 1.0,
+    "Media Pontos por Jogo Casa": 2.0,
+    "Media Pontos por Jogo Fora": 0.3333333333333333,
+    "Total Pontos Casa": 4,
+    "Total Pontos Fora": 1
+  },
+  {
+    "Clube ID": "FLA",
+    "Media Pontos por Jogo": 1.3333333333333333,
+    "Media Pontos por Jogo Casa": 1.0,
+    "Media Pontos por Jogo Fora": 1.5,
+    "Total Pontos Casa": 1,
+    "Total Pontos Fora": 3
+  },
+  {
+    "Clube ID": "CFC",
+    "Media Pontos por Jogo": 1.0,
+    "Media Pontos por Jogo Casa": 0.0,
+    "Media Pontos por Jogo Fora": 2.0,
+    "Total Pontos Casa": 0,
+    "Total Pontos Fora": 4
+  },
+  {
+    "Clube ID": "BOT",
+    "Media Pontos por Jogo": 1.0,
+    "Media Pontos por Jogo Casa": 3.0,
+    "Media Pontos por Jogo Fora": 0.0,
+    "Total Pontos Casa": 3,
+    "Total Pontos Fora": 0
   },
   {
     "Clube ID": "VIT",
-    "Media Pontos por Jogo": 1.0689655172413792,
-    "Media Pontos por Jogo Casa": 1.6428571428571428,
-    "Media Pontos por Jogo Fora": 0.5333333333333333,
-    "Total Pontos Casa": 23,
-    "Total Pontos Fora": 8
+    "Media Pontos por Jogo": 1.0,
+    "Media Pontos por Jogo Casa": 1.5,
+    "Media Pontos por Jogo Fora": 0.0,
+    "Total Pontos Casa": 3,
+    "Total Pontos Fora": 0
   },
   {
-    "Clube ID": "JUV",
-    "Media Pontos por Jogo": 0.896551724137931,
-    "Media Pontos por Jogo Casa": 1.4,
-    "Media Pontos por Jogo Fora": 0.35714285714285715,
-    "Total Pontos Casa": 21,
-    "Total Pontos Fora": 5
+    "Clube ID": "REM",
+    "Media Pontos por Jogo": 0.75,
+    "Media Pontos por Jogo Casa": 1.0,
+    "Media Pontos por Jogo Fora": 0.5,
+    "Total Pontos Casa": 2,
+    "Total Pontos Fora": 1
   },
   {
-    "Clube ID": "FOR",
-    "Media Pontos por Jogo": 0.8571428571428571,
-    "Media Pontos por Jogo Casa": 1.1333333333333333,
-    "Media Pontos por Jogo Fora": 0.5384615384615384,
-    "Total Pontos Casa": 17,
-    "Total Pontos Fora": 7
+    "Clube ID": "CAM",
+    "Media Pontos por Jogo": 0.5,
+    "Media Pontos por Jogo Casa": 1.0,
+    "Media Pontos por Jogo Fora": 0.0,
+    "Total Pontos Casa": 2,
+    "Total Pontos Fora": 0
   },
   {
-    "Clube ID": "SPT",
-    "Media Pontos por Jogo": 0.6071428571428571,
-    "Media Pontos por Jogo Casa": 0.7692307692307693,
-    "Media Pontos por Jogo Fora": 0.4666666666666667,
-    "Total Pontos Casa": 10,
-    "Total Pontos Fora": 7
+    "Clube ID": "INT",
+    "Media Pontos por Jogo": 0.5,
+    "Media Pontos por Jogo Casa": 0.0,
+    "Media Pontos por Jogo Fora": 1.0,
+    "Total Pontos Casa": 0,
+    "Total Pontos Fora": 2
+  },
+  {
+    "Clube ID": "CRU",
+    "Media Pontos por Jogo": 0.5,
+    "Media Pontos por Jogo Casa": 0.5,
+    "Media Pontos por Jogo Fora": 0.5,
+    "Total Pontos Casa": 1,
+    "Total Pontos Fora": 1
+  },
+  {
+    "Clube ID": "VAS",
+    "Media Pontos por Jogo": 0.25,
+    "Media Pontos por Jogo Casa": 0.5,
+    "Media Pontos por Jogo Fora": 0.0,
+    "Total Pontos Casa": 1,
+    "Total Pontos Fora": 0
   }
 ];
